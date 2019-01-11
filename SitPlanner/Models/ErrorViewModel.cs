c4436634a4fc3d6217773eb6d5faa602bf49ea93
Talia Mazor <tmazor@paloaltonetworks.com>
2019-01-11 11:38:11 +0200

    initial project

---
+++ SitPlanner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SitPlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
