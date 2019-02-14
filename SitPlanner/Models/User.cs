1752f614f651fa61dc3aae5336a82d95d47b0e85
lvaknin <lee.vaknin8@gmail.com>
2019-02-14 16:42:36 +0200

    Start handling login

--- SitPlanner/Models/User.cs
+++ SitPlanner/Models/User.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,19 @@ namespace SitPlanner.Models
 {
     public class User : IdentityUser<int>
     {
+        [Required(ErrorMessage = "Please provide first name")]
+        [Display(Name = "First name")]
+        [StringLength(50, MinimumLength = 1)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please provide last name")]
+        [Display(Name = "Last name")]
+        [StringLength(50, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => String.Join(" ", FirstName, LastName);
+
         public List<Event> Events { get; set; }
     }
 }
