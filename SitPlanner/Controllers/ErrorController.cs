b56ce457d8b89cc20a698f5d85e20ce9e4cded20
Lee Vaknin <lee@infinigrow.com>
2019-05-13 15:51:33 +0300

    Added error pages

---
+++ SitPlanner/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SitPlanner.Controllers
+{
+    [Route("Error")]
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        [HttpGet("/Error/{errorId}")]
+        public IActionResult InvalidAction(int errorId)
+        {
+            if (errorId == 401 || errorId == 403)
+                return View("Views/Error/InvalidAction.cshtml");
+            return OurError();
+        }
+
+        [Route("")]
+        [Route("/Error")]
+        public IActionResult OurError()
+        {
+            return View("Views/Error/OurError.cshtml");
+        }
+
+        [Route("/Error/InvalidAction")]
+        public IActionResult InvalidAction()
+        {
+            return View("Views/Error/InvalidAction.cshtml");
+        }
+    }
+}
