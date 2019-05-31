8eb5740db2e5056c96b35e309495c25272810111
Talia Mazor <tmazor@paloaltonetworks.com>
2019-05-31 23:55:39 +0300

    Update AlgoDb.cs

--- SitPlanner/Algo/AlgoDb.cs
+++ SitPlanner/Algo/AlgoDb.cs
@@ -8,20 +8,18 @@ namespace SitPlanner.Algo
 {
     public class AlgoDb
     {
-        public List<Invitee> invitees = new List<Invitee>();
-        public List<Table> tables = new List<Table>();
-        public List<PersonalRestriction> personalRestrictions = new List<PersonalRestriction>();
-        public List<AccessibilityRestriction> accessibilityRestrictions = new List<AccessibilityRestriction>();
-        public List<Category> categories = new List<Category>();
+        public List<Invitee> invitees;
+        public List<Table> tables;
+        public List<PersonalRestriction> personalRestrictions;
+        public List<AccessibilityRestriction> accessibilityRestrictions;
 
         public AlgoDb(List<Invitee> invitees, List<Table> tables, List<PersonalRestriction> personalRestrictions, 
             List<AccessibilityRestriction> accessibilityRestrictions, List<Category> categories)
         {
-            this.invitees = invitees;
-            this.tables = tables;
-            this.personalRestrictions = personalRestrictions;
-            this.accessibilityRestrictions = accessibilityRestrictions;
-            this.categories = categories;
+            this.invitees = new List<Invitee>(invitees);
+            this.tables = new List<Table>(tables);
+            this.personalRestrictions = new List<PersonalRestriction>(personalRestrictions);
+            this.accessibilityRestrictions = new List<AccessibilityRestriction>(accessibilityRestrictions);
         }
     }
 }
