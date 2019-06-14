ccf9349a06d0655e8a0915edfcdf9367ae1e8d7f
Lee Vaknin <lee@infinigrow.com>
2019-06-14 19:04:28 +0300

    added migration for deleting event option at personal restriction

--- SitPlanner/Migrations/20190614160212_removedEventOptionFromPersonalRestriction.Designer.cs
+++ SitPlanner/Migrations/20190614160212_removedEventOptionFromPersonalRestriction.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SitPlanner.Data;
 
 namespace SitPlanner.Migrations
 {
     [DbContext(typeof(SitPlannerContext))]
-    partial class SitPlannerContextModelSnapshot : ModelSnapshot
+    [Migration("20190614160212_removedEventOptionFromPersonalRestriction")]
+    partial class removedEventOptionFromPersonalRestriction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
