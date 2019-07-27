920f2790af0e8a664d7e00946001f443c6abef2b
Lee Vaknin <lee@infinigrow.com>
2019-07-27 17:46:20 +0300

    added accessibility restriction for table type. added page add fixed aldo func

---
+++ SitPlanner/Migrations/20190727135412_removedEventOptionFromAccessibilityRestriction.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SitPlanner.Migrations
+{
+    public partial class removedEventOptionFromAccessibilityRestriction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AccessibilityRestriction_EventOption_EventOptionId",
+                table: "AccessibilityRestriction");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventOptionId",
+                table: "AccessibilityRestriction",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AccessibilityRestriction_EventOption_EventOptionId",
+                table: "AccessibilityRestriction",
+                column: "EventOptionId",
+                principalTable: "EventOption",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AccessibilityRestriction_EventOption_EventOptionId",
+                table: "AccessibilityRestriction");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EventOptionId",
+                table: "AccessibilityRestriction",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AccessibilityRestriction_EventOption_EventOptionId",
+                table: "AccessibilityRestriction",
+                column: "EventOptionId",
+                principalTable: "EventOption",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
